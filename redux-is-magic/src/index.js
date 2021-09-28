b5b50be8d659d7aba2b7f39db547663b02657451
= <roulit77@gmail.com>
2021-09-28 10:52:24 +0200

    Solution done

--- redux-is-magic/src/index.js
+++ redux-is-magic/src/index.js
@@ -1,4 +1,34 @@
-import React from 'react';
+import { createStore } from 'redux'
+
+let initialState = {
+  greetings: "Not In the moood!"
+}
+function greetingsReducer(state = initialState, action) {
+  switch (action.type) {
+    case 'greetings/sayHello':
+      return { greetings: "Hello world!" }
+    case 'greetings/sayGoodbye':
+      return { greetings: "Goodbye!" }
+    default:
+      return state
+  }
+}
+
+let store = createStore(greetingsReducer)
+
+store.subscribe(() => console.log(store.getState()))
+
+store.dispatch({ type: 'greetings/sayHello' })
+// {greetings: "Hello world!"}
+store.dispatch({ type: 'greetings/sayHello' })
+// {greetings: "Hello world!"}
+store.dispatch({ type: 'greetings/sayGoodbye' })
+// {greetings: "Goodbye!"}
+
+
+
+
+/* import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
@@ -14,4 +44,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(); */
